b0bddd42ca9a7a5c66cc3d574b4f8f25bca5340f
Storm00212 <paulmuyalikhams@gmail.com>
2026-05-22 10:09:27 +0300

    eslint rules

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+const globals = require("globals");
+
+module.exports = [
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {},
+  },
+];
